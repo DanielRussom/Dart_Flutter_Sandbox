c0184d79ba3ad8352346267089f6f29f02f777c0
Daniel Russom <daniel.russom00@gmail.com>
2022-10-10 11:51:53 +0100

    Init

---
+++ test/roman_numerals_test.dart
@@ -0,0 +1,70 @@
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hello_world/main.dart';
+
+void main() {
+  final testParameters = {
+    1: 'I',
+    2: 'II',
+    3: 'III',
+    4: 'IV',
+    5: 'V',
+    6: 'VI',
+    7: 'VII',
+    8: 'VIII',
+    9: 'IX',
+    10: 'X'
+  };
+
+  for (var parameterSet in testParameters.entries) {
+    test('Convert ${parameterSet.key} to ${parameterSet.value} ', () {
+      final result = convertToRomanNumeral(parameterSet.key);
+
+      expect(result, equals(parameterSet.value));
+    });
+  }
+
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+
+    expect(find.text('Generate Roman Numeral'), findsOneWidget);
+  });
+}
+
+String convertToRomanNumeral(int input) {
+  var result = '';
+
+  for (var numeral in RomanNumerals.values.reversed) {
+    while (input >= numeral.toArabic()) {
+      result += numeral.name;
+      input -= numeral.toArabic();
+    }
+  }
+
+  return result;
+}
+
+enum RomanNumerals {
+  I,
+  IV,
+  V,
+  IX,
+  X,
+}
+
+extension RomanNumeralExtension on RomanNumerals {
+  int toArabic() {
+    switch (this) {
+      case RomanNumerals.I:
+        return 1;
+      case RomanNumerals.IV:
+        return 4;
+      case RomanNumerals.V:
+        return 5;
+      case RomanNumerals.IX:
+        return 9;
+      case RomanNumerals.X:
+        return 10;
+    }
+  }
+}
