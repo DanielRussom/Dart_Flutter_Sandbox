5f1e0d158db56ba235dc909cab78d3db2cb9e4dd
Daniel Russom <daniel.russom00@gmail.com>
2022-11-02 17:26:47 +0000

    Setup Registration Acceptance test outline

---
+++ dart_flutter_sandbox/test/modules/user_register_acceptance_test.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Registration feature should', () {
+    testWidgets('register new user', (WidgetTester tester) async {
+      // A: Click register button
+      // E: Be on registration page
+
+      // A: Enter username and password
+      // A: Click Register button
+      // E: Navigate to Logged-In page
+      // E: "Hello, {Username}" displayed on screen
+    });
+  });
+}
